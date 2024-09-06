ff5cf67e32d7ec81a7d37bafc9d9c1353ac7d8d0
Ahany42 <ahanyfathy42@gmail.com>
2024-09-06 12:01:30 +0300

    Placeholder Component

---
+++ FrontEnd/src/Components/Placeholder/Placeholder.jsx
@@ -0,0 +1,15 @@
+import './Placeholder.css'
+const Placeholder = ({img,text}) => {
+  return (
+    <>
+     <div className="PageComponent">
+ <img src={img} className="PlaceholderImage" alt="Placeholder"/>
+ <div className="TextContainer">
+ <h3>{text}</h3>
+ </div>
+  </div>
+    </>
+  )
+}
+
+export default Placeholder
