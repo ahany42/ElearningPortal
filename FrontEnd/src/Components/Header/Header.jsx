85f4e3007763b92efc24621b036d48a6a641c87d
hamzzawy74 <hamza.elgamily10@gmail.com>
2024-09-13 21:26:06 +0300

    Add: media quires for header tabs

--- FrontEnd/src/Components/Header/Header.jsx
+++ FrontEnd/src/Components/Header/Header.jsx
@@ -1,16 +1,29 @@
-import "./Header.css"
+import "./Header.css";
+import { useState } from "react";
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
     return (
         <div className="navbar">
-            <li>
-                <a href="#">Home</a>
-                <a href="Courses">Courses</a>
-                <a href="Deadlines">Deadlines</a>
-            </li>
+            <ul className={menuOpen ? "show" : ""}>
+                <li><a href="#">Home</a></li>
+                <li><a href="Courses">Courses</a></li>
+                <li><a href="Deadlines">Deadlines</a></li>
+            </ul>
+            <button className="toggle-menu" onClick={toggleMenu}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
         </div>
-    )
+    );
 }
 
 export default Header;
+
+
