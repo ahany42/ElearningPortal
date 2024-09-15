3ab3b46ae00f1ade6187cb187dbd7ef747820632
Eng-Khalid-Zaki <eng.khalid.zaki.2022@gmail.com>
2024-09-15 03:13:09 +0300

    Finishing User Controller and User Routes

--- Back End/router/UserRouter.js
+++ Back End/router/UserRouter.js
@@ -7,12 +7,12 @@ const router = Router();
  */
 
 router.post("/login", Controller.login); // verified
-router.post("/logout", Controller.logout);
+router.post("/logout", Controller.logout); //verified
 router.post("/register", Controller.register); // verified
 router.get("/getUser", Controller.getUser); // verified
 router.get("/getUsers", Controller.getUsers); // verified
-router.put("/updateUser", Controller.updateUser);
-router.post("/deleteUser", Controller.deleteUser);  // verified
-router.post("/forgotPassword", Controller.forgotPassword);  // verified
+router.put("/updateUser/:id", Controller.updateUser); //verified
+router.post("/deleteUser", Controller.deleteUser); // verified
+router.post("/forgotPassword", Controller.forgotPassword); // verified
 
 module.exports = router;
