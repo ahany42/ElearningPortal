eb8faecdf172e8ce4133237cade164e725e8c3d1
Omar Alaa Elnahass <21p0197@eng.asu.edu.eg>
2024-09-22 23:10:44 +0300

    edit: homepage design with header and footer, add: favicon and tab title

--- FrontEnd/src/Components/Header/Header.jsx
+++ FrontEnd/src/Components/Header/Header.jsx
@@ -1,7 +1,32 @@
 import IconButton from "../IconButton/IconButton";
+import MuiIconButton from '@mui/material/IconButton';
 import "./Header.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import {useState} from "react";
+import {NavLink} from "react-router-dom";
+import {v4} from "uuid";
+
+const pages = [
+    { name: 'Home', to: '/'},
+    { name: 'Courses', to: '/Courses'},
+    { name: 'Login / Register', to: '/login'},
+];
 
 const Header = () => {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
     <header className="header">
       <nav className="navbar">
@@ -11,6 +36,47 @@ const Header = () => {
           <IconButton label="Courses" to="/Courses" />
           <IconButton label="Login / Register" to="/login" />
         </div>
+        <Box sx={{ display:
+              { xs: 'flex', sm: 'flex', md: 'none',
+                '@media (max-width: 768px)': { display: 'flex' }, '@media (min-width: 769px)': { display: 'none' } } }}>
+          <MuiIconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+          >
+            <MenuIcon />
+          </MuiIconButton>
+          <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: 'block', md: 'none', '@media (max-width: 768px)':
+                      { display: 'block' }, '@media (min-width: 769px)': { display: 'none' } } }}
+          >
+            {pages.map((page) => (
+                <NavLink key={v4()} to={page.to} style={{textDecoration: 'none'}}>
+                  <MenuItem onClick={handleCloseNavMenu}>
+                    <Typography sx={{ textAlign: 'center' }}>
+                      {page.name}
+                    </Typography>
+                  </MenuItem>
+                </NavLink>
+            ))}
+          </Menu>
+        </Box>
       </nav>
     </header>
   );
--- FrontEnd/src/Components/HomePage/HomePage.jsx
+++ FrontEnd/src/Components/HomePage/HomePage.jsx
@@ -1,11 +1,74 @@
+import { useState } from 'react';
 import './HomePage.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FirstCarousel from '../../assets/Login.svg';
+import SecondCarousel from '../../assets/Grades.svg';
+import ThirdCarousel from '../../assets/Student.svg';
+import LeftPart from '../../assets/left-part-background.png';
+import { Carousel } from 'react-bootstrap';
 
 const HomePage = () => {
+    const [anchorElNav, setAnchorElNav] = useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
     return (
-        <div className='home-container'></div>
-        
-    )
-}
+        <div className="home-container">
+            <img className='leftPart' draggable='false' src={LeftPart} alt="Office" />
+            <div className="CarouselContainer">
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            style={{height: '256px'}}
+                            src={FirstCarousel}
+                            alt="First slide"
+                            draggable='false'
+                        />
+                        <div className="CarouselText">
+                            <h3>First Slide Label</h3>
+                            <p>Some text for the first slide.</p>
+                        </div>
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            style={{height: '256px'}}
+                            src={SecondCarousel}
+                            alt="Second slide"
+                            draggable='false'
+                        />
+                        <div className="CarouselText">
+                            <h3>Second Slide Label</h3>
+                            <p>Some text for the second slide.</p>
+                        </div>
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            style={{height: '256px'}}
+                            src={ThirdCarousel}
+                            alt="Third slide"
+                            draggable='false'
+                        />
+                        <div className="CarouselText">
+                            <h3>Third Slide Label</h3>
+                            <p>Some text for the third slide.</p>
+                        </div>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+        </div>
+    );
+};
 
 export default HomePage
+
