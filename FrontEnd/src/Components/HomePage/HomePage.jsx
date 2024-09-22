369009392ab961ae19db39557ded2c07355c89a0
farah-yehia <yehiafarah60@gmail.com>
2024-09-22 23:59:01 +0300

    done:homepage

--- FrontEnd/src/Components/HomePage/HomePage.jsx
+++ FrontEnd/src/Components/HomePage/HomePage.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import './HomePage.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import FirstCarousel from '../../assets/Login.svg';
-import SecondCarousel from '../../assets/Grades.svg';
-import ThirdCarousel from '../../assets/Student.svg';
+import FirstCarousel from '../../assets/react.webp';
+import SecondCarousel from '../../assets/node.webp';
+import ThirdCarousel from '../../assets/mongo.webp';
 import LeftPart from '../../assets/left-part-background.png';
 import { Carousel } from 'react-bootstrap';
 
@@ -19,54 +19,67 @@ const HomePage = () => {
     };
 
     return (
-        <div className="home-container">
-            <img className='leftPart' draggable='false' src={LeftPart} alt="Office" />
-            <div className="CarouselContainer">
-                <Carousel>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            style={{height: '256px'}}
-                            src={FirstCarousel}
-                            alt="First slide"
-                            draggable='false'
-                        />
-                        <div className="CarouselText">
-                            <h3>First Slide Label</h3>
-                            <p>Some text for the first slide.</p>
-                        </div>
-                    </Carousel.Item>
+      <div className="home-container">
+        <img
+          className="leftPart"
+          draggable="false"
+          src={LeftPart}
+          alt="Office"
+        />
+        <div className="CarouselContainer">
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                style={{ height: "256px" }}
+                src={FirstCarousel}
+                alt="First slide"
+                draggable="false"
+              />
+              <div className="CarouselText">
+                <h3>Learn React</h3>
+                <p>
+                  Master front-end development with the most popular JavaScript
+                  library.
+                </p>
+              </div>
+            </Carousel.Item>
 
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            style={{height: '256px'}}
-                            src={SecondCarousel}
-                            alt="Second slide"
-                            draggable='false'
-                        />
-                        <div className="CarouselText">
-                            <h3>Second Slide Label</h3>
-                            <p>Some text for the second slide.</p>
-                        </div>
-                    </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                style={{ height: "256px" }}
+                src={SecondCarousel}
+                alt="Second slide"
+                draggable="false"
+              />
+              <div className="CarouselText">
+                <h3>Node.js Essentials</h3>
+                <p>
+                  Build fast and scalable back-end applications with Node.js.
+                </p>
+              </div>
+            </Carousel.Item>
 
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            style={{height: '256px'}}
-                            src={ThirdCarousel}
-                            alt="Third slide"
-                            draggable='false'
-                        />
-                        <div className="CarouselText">
-                            <h3>Third Slide Label</h3>
-                            <p>Some text for the third slide.</p>
-                        </div>
-                    </Carousel.Item>
-                </Carousel>
-            </div>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                style={{ height: "256px" }}
+                src={ThirdCarousel}
+                alt="Third slide"
+                draggable="false"
+              />
+              <div className="CarouselText">
+                <h3>Master MongoDB</h3>
+                <p>
+                  Store and manage data effectively with this powerful NoSQL
+                  database.
+                </p>
+              </div>
+            </Carousel.Item>
+          </Carousel>
         </div>
+      </div>
     );
 };
 
