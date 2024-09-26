6e93a8698ac50c0b39ee8c921a9f671526e7226c
Ahany42 <ahanyfathy42@gmail.com>
2024-09-26 16:23:09 +0300

    Exam material

--- FrontEnd/src/Components/CourseMaterial/CourseMaterial.jsx
+++ FrontEnd/src/Components/CourseMaterial/CourseMaterial.jsx
@@ -1,28 +1,41 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBookOpen } from '@fortawesome/free-solid-svg-icons';
+import { faBookOpen , faFileAlt } from '@fortawesome/free-solid-svg-icons';
 import './CourseMaterial.css';
 const CourseMaterial = ({materialType}) => {
+    //for testing
     const submitted = false;
+    //for testing
+    const solved = true;
   return (
   <>
     {/* for testing */}
     { materialType === "exam"?(
     <div className=" card material-card ">
     <div className=" material-sub-card">
-    <FontAwesomeIcon className="material-icon" size="3x" icon={faBookOpen} color="#274546"/>
+    <FontAwesomeIcon className="material-icon" size="3x" icon={faFileAlt} color="#274546"/>
     <div>
     <div className="material-title-due">
-    <h6>Aly Hany posted a new assignment :Assignment 1</h6>
+    <h6>Aly Hany posted a new exam :Exam 1</h6>
     </div>
     <h6 className="material-date">26th Sep 24 - 27th Sep 24 </h6>
     </div>
    
-    {submitted? <h6 className="submit-material blue-text bold-text">Submitted</h6>:(<button className="submit-material blue-text bold-text">Submit</button>)}
+    {!solved? <button className="material-button blue-text bold-text">Solve</button>:(<button className="material-button blue-text bold-text">See Grade</button>)}
     </div>
     </div>
     ):materialType=== "assignment"?(
-        <div className="material-card">
-
+        <div className=" card material-card ">
+        <div className=" material-sub-card">
+        <FontAwesomeIcon className="material-icon" size="3x" icon={faBookOpen} color="#274546"/>
+        <div>
+        <div className="material-title-due">
+        <h6>Aly Hany posted a new assignment :Assignment 1</h6>
+        </div>
+        <h6 className="material-date">26th Sep 24 - 27th Sep 24 </h6>
+        </div>
+       
+        {submitted? <h6 className="material-button blue-text bold-text">Submitted</h6>:(<button className="material-button blue-text bold-text">Submit</button>)}
+        </div>
         </div>
     ):materialType === "announcement"?(
         <div className="material-card">
