b0d6725869cf7675cddecef6583d2babee342a59
hamzzawy74 <hamza.elgamily10@gmail.com>
2024-09-30 21:37:04 +0300

    updated styling on profile page #1

--- FrontEnd/src/Components/UserProfile/UserProfile.jsx
+++ FrontEnd/src/Components/UserProfile/UserProfile.jsx
@@ -1,20 +1,19 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import "./UserProfile.css";
+import './UserProfile.css';
+import avatar from '../../assets/avatar.jpg';
 
 const UserProfile = () => {
-  const navigate = useNavigate ();
+  const navigate = useNavigate();
   const [isEditing, setIsEditing] = useState(false);
-  const [isChangingPassword, setIsChangingPassword] = useState(false); 
-
+  const [isChangingPassword, setIsChangingPassword] = useState(false);
 
   const [profileData, setProfileData] = useState({
     name: 'John Doe',
     username: 'johndoe123',
     email: 'johndoe@example.com',
-    password: '', 
+    password: '',
   });
-
 
   const handleChange = (e) => {
     setProfileData({
@@ -23,83 +22,88 @@ const UserProfile = () => {
     });
   };
 
- 
   const toggleEditMode = () => {
     setIsEditing(!isEditing);
   };
 
   const changePassword = () => {
     navigate('/ResetPassword');
-  }
+  };
 
   return (
     <>
       <h1>User Profile</h1>
-
-      
-      <div>
-        
-      </div>
-      <div className='user-data'>
-        <label>Name: </label>
-        <input
-          className={`data-input ${!isEditing ? 'read-only' : ''}`} 
-          type="text"
-          name="name"
-          value={profileData.name}
-          onChange={handleChange}
-          readOnly={!isEditing}
-        />
-      </div>
-
-      <div className='user-data'>
-        <label>Username: </label>
-        <input
-          className={`data-input ${!isEditing ? 'read-only' : ''}`} 
-          type="text"
-          name="username"
-          value={profileData.username}
-          onChange={handleChange}
-          readOnly={!isEditing}
-        />
-      </div>
-
-      <div className='user-data'>
-        <label>Email: </label>
-        <input
-          className={`data-input ${!isEditing ? 'read-only' : ''}`} 
-          type="email"
-          name="email"
-          value={profileData.email}
-          onChange={handleChange}
-          readOnly={!isEditing}
-        />
-      </div>
-
-      
-      {isChangingPassword && (
-        <div className='user-data'>
-          <label>Password: </label>
-          <input
-            className='data-input'
-            type="password"
-            name="password"
-            value={profileData.password}
-            onChange={handleChange}
-            placeholder="Enter new password"
-          />
+      <div className="profile-container">
+        <div className="profile-picture green-bg light-text">
+          <img src={avatar} alt="User" />
+          <p>Student</p>
         </div>
-      )}
+        <div className="profile-details">
+          <div className="user-data-container green-bg">
+            <div className="user-data">
+              <label>Name: </label>
+              <input
+                className={`data-input ${!isEditing ? 'read-only' : ''}`}
+                type="text"
+                name="name"
+                value={profileData.name}
+                onChange={handleChange}
+                readOnly={!isEditing}
+              />
+            </div>
 
-      
-      <div className='button-group'>
-        <button className='edit green-bg light-text' onClick={toggleEditMode}>
-          {isEditing ? 'Save' : 'Edit'}
-        </button>
+            <div className="user-data">
+              <label>Username: </label>
+              <input
+                className={`data-input ${!isEditing ? 'read-only' : ''}`}
+                type="text"
+                name="username"
+                value={profileData.username}
+                onChange={handleChange}
+                readOnly={!isEditing}
+              />
+            </div>
 
-        <button className='change-password green-bg light-text' onClick={changePassword}>
-          {isChangingPassword ? 'Save Password' : 'Change Password'}
-        </button>
+            <div className="user-data">
+              <label>Email: </label>
+              <input
+                className={`data-input ${!isEditing ? 'read-only' : ''}`}
+                type="email"
+                name="email"
+                value={profileData.email}
+                onChange={handleChange}
+                readOnly={!isEditing}
+              />
+            </div>
+
+            {isChangingPassword && (
+              <div className="user-data">
+                <label>Password: </label>
+                <input
+                  className="data-input"
+                  type="password"
+                  name="password"
+                  value={profileData.password}
+                  onChange={handleChange}
+                  placeholder="Enter new password"
+                />
+              </div>
+            )}
+          </div>
+
+          <div className="button-group">
+            <button className="edit green-bg light-text" onClick={toggleEditMode}>
+              {isEditing ? 'Save' : 'Edit'}
+            </button>
+
+            <button
+              className="change-password green-bg light-text"
+              onClick={changePassword}
+            >
+              {isChangingPassword ? 'Save Password' : 'Change Password'}
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
