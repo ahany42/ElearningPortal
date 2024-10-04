58f2639ba5e545428c95be27131be05fa48e5194
Eng-Khalid-Zaki <eng.khalid.zaki.2022@gmail.com>
2024-10-04 23:58:34 +0300

    Finishing ExamController

--- BackEnd/controller/Exam.js
+++ BackEnd/controller/Exam.js
@@ -2,13 +2,16 @@ const { Exam, StudentExam, Question, Course } = require("../db/Database");
 const { v4: uuidv4 } = require("uuid");
 /*
     Functions to be implemented:
-    - createExam  DONE
-    - addQuestions DONE
-    - getExams 
-    - getExam
-    - updateExam
-    - SolveExam
-    - getStudentExams
+    - createExam               DONE
+    - addQuestions             DONE
+    - getExams                 DONE
+    - getExam                  DONE
+    - updateExam               DONE
+    - updateQuestion           DONE
+    - deleteExam               DONE
+    - deleteQuestion           DONE
+    - SolveExam                DONE
+    - getStudentExams          DONE
 */
 async function findCOurseIdByTitle(title) {
   return Course.findOne({ title: title }).then((course) => {
@@ -96,4 +99,187 @@ module.exports.addQuestions = async (req, res, next) => {
   }
 };
 
-module.exports.getExam = async (req, res, next) => {};
+module.exports.getExamsforAdmins = async (req, res, next) => {
+  try {
+    const exams = await Exam.find();
+    res.status(201).json({ data: exams });
+  } catch (error) {
+    res.status(200).json({ error: "Unexpected Error Occurred" });
+    next(`ERROR IN: getExams function => ${error}`);
+  }
+};
+
+module.exports.getExamsforStudents = async (req, res, next) => {
+  try {
+    const { studentId } = req.params;
+    if (!studentId) {
+      return res.status(200).json({ error: "Student ID is required" });
+    }
+    const studentExams = await StudentExam.find({ studentID: studentId });
+    res.status(201).json({ data: studentExams });
+  } catch (error) {
+    res.status(200).json({ error: "Unexpected Error Occurred" });
+    next(`ERROR IN: getExams function => ${error}`);
+  }
+};
+
+module.exports.getExam = async (req, res, next) => {
+  try {
+    const { examId } = req.params;
+    if (!examId) {
+      return res.status(200).json({ error: "Exam ID is required" });
+    }
+    const exam = await Exam.findOne({ id: examId });
+    if (!exam) {
+      return res.status(200).json({ error: "Exam not found" });
+    }
+    res.status(201).json({ data: exam });
+  } catch (error) {
+    res.status(200).json({ error: "Unexpected Error Occurred" });
+    next(`ERROR IN: getExam function => ${error}`);
+  }
+};
+
+module.exports.deleteExam = async (req, res, next) => {
+  try {
+    const { examId } = req.params;
+    if (!examId) {
+      return res.status(200).json({ error: "Exam ID is required" });
+    }
+    const exam = await Exam.findOne({ id: examId });
+    if (!exam) {
+      return res.status(200).json({ error: "Exam not found" });
+    }
+    await Exam.deleteOne({ id: examId });
+    res.status(201).json({ message: "Exam deleted successfully" });
+  } catch (error) {
+    res.status(200).json({ error: "Unexpected Error Occurred" });
+    next(`ERROR IN: deleteExam function => ${error}`);
+  }
+};
+
+module.exports.updateExam = async (req, res, next) => {
+  try {
+    const { examId } = req.params;
+    if (!examId) {
+      return res.status(200).json({ error: "Exam ID is required" });
+    }
+    const exam = await Exam.findOne({ id: examId });
+    if (!exam) {
+      return res.status(200).json({ error: "Exam not found" });
+    }
+    const { title, startDate, duration, endDate } = req.body;
+    if (!title || !startDate || !duration || !endDate) {
+      return res.status(200).json({ error: "All fields are required" });
+    }
+    if (exam.title != title) {
+      exam.title = title;
+    }
+    if (exam.startDate != startDate) {
+      exam.startDate = startDate;
+    }
+    if (exam.duration != duration) {
+      exam.duration = duration;
+    }
+    if (exam.endDate != endDate) {
+      exam.endDate = endDate;
+    }
+    await exam.save();
+    res.status(201).json({ message: "Exam updated successfully", exam });
+  } catch (error) {
+    res.status(200).json({ error: "Unexpected Error Occurred" });
+    next(`ERROR IN: updateExam function => ${error}`);
+  }
+};
+
+module.exports.updateQuestion = async (req, res, next) => {
+  try {
+    const { questionId } = req.params;
+    if (!questionId) {
+      return res.status(200).json({ error: "Question ID is required" });
+    }
+    const question = await Question.findOne({ id: questionId });
+    if (!question) {
+      return res.status(200).json({ error: "Question not found" });
+    }
+    const { title, answers, correctAnswer } = req.body;
+    if (!title || !answers || !correctAnswer) {
+      return res.status(200).json({ error: "All fields are required" });
+    }
+    if (question.title != title) {
+      question.title = title;
+    }
+    if (question.answers != answers) {
+      question.answers = answers;
+    }
+    if (question.correctAnswer != correctAnswer) {
+      question.correctAnswer = correctAnswer;
+    }
+    await question.save();
+    res
+      .status(201)
+      .json({ message: "Question updated successfully", question });
+  } catch (error) {
+    res.status(200).json({ error: "Unexpected Error Occurred" });
+    next(`ERROR IN: updateQuestion function => ${error}`);
+  }
+};
+
+module.exports.deleteQuestion = async (req, res, next) => {
+  try {
+    const { questionId } = req.params;
+    if (!questionId) {
+      return res.status(200).json({ error: "Question ID is required" });
+    }
+    const question = await Question.findOne({ id: questionId });
+    if (!question) {
+      return res.status(200).json({ error: "Question not found" });
+    }
+    await Question.deleteOne({ id: questionId });
+    res.status(201).json({ message: "Question deleted successfully" });
+  } catch (error) {
+    res.status(200).json({ error: "Unexpected Error Occurred" });
+    next(`ERROR IN: deleteQuestion function => ${error}`);
+  }
+};
+
+module.exports.solveExam = async (req, res, next) => {
+  try {
+    const { examId } = req.params;
+    if (!examId) {
+      return res.status(200).json({ error: "Exam ID is required" });
+    }
+    const exam = await Exam.findOne({ id: examId });
+    if (!exam) {
+      return res.status(200).json({ error: "Exam not found" });
+    }
+    const { studentId, grade, submissionDate } = req.body;
+    if (!studentId || !grade || !submissionDate) {
+      return res.status(200).json({ error: "All fields are required" });
+    }
+    const student = await User.findOne({ id: studentId });
+    if (!student) {
+      return res.status(200).json({ error: "Student not found" });
+    }
+    const studentExam = await Student_Exam.findOne({
+      examID: examId,
+      studentID: studentId,
+    });
+    if (!studentExam) {
+      return res
+        .status(200)
+        .json({ error: "Student not registered for this exam" });
+    }
+    if (submissionDate > exam.endDate) {
+      studentExam.grade = 0;
+    } else {
+      studentExam.grade = grade;
+    }
+
+    await studentExam.save();
+    res.status(201).json({ message: "Exam solved successfully" });
+  } catch (error) {
+    res.status(200).json({ error: "Unexpected Error Occurred" });
+    next(`ERROR IN: solveExam function => ${error}`);
+  }
+};
