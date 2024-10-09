6d45d14cd31dee90bfb2398a9e6310ab05cd45ae
Eng-Khalid-Zaki <eng.khalid.zaki.2022@gmail.com>
2024-10-09 07:52:54 +0300

    Updating some changes in backend

--- BackEnd/db/Database.js
+++ BackEnd/db/Database.js
@@ -5,211 +5,298 @@ const { Secret_Key } = require("../../env");
 
 // Connect to MongoDB
 mongoose
-    .connect("mongodb://localhost:27017/e-learning")
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.log(err));
+  .connect("mongodb://localhost:27017/e-learning")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 // Create the User model
 const User = mongoose.model(
-    "User",
-    new mongoose.Schema(
-        {
-            name: { type: String, required: true },
-            id: { type: String, required: true },
-            gender: {
-                type: String,
-                enum: ["Male", "Female"],
-                required: true,
-            },
-            email: { type: String, required: true },
-            username: { type: String, required: true, unique: true },
-            password: { type: String, required: true },
-            role: {
-                type: String,
-                enum: ["SuperAdmin", "Admin", "Instructor", "Student"],
-                required: true,
-            },
-            resetPasswordToken: String,
-            resetPasswordExpires: Date,
-        },
-        { timestamps: true }
-    )
+  "User",
+  new mongoose.Schema(
+    {
+      name: { type: String, required: true },
+      id: { type: String, required: true },
+      gender: {
+        type: String,
+        enum: ["Male", "Female"],
+        required: true,
+      },
+      email: { type: String, required: true },
+      username: { type: String, required: true, unique: true },
+      password: { type: String, required: true },
+      role: {
+        type: String,
+        enum: ["SuperAdmin", "Admin", "Instructor", "Student"],
+        required: true,
+      },
+      resetPasswordToken: String,
+      resetPasswordExpires: Date,
+    },
+    { timestamps: true }
+  )
 );
 
 const Course = mongoose.model(
-    "Course",
-    new mongoose.Schema(
-        {
-            title: { type: String, required: true, unique: true },
-            desc: { type: String, required: true },
-            id: { type: String, required: true, unique: true },
-            hours: { type: Number, required: true },
-            image: { type: String },
-        },
-        { timestamps: true }
-    )
+  "Course",
+  new mongoose.Schema(
+    {
+      title: { type: String, required: true, unique: true },
+      desc: { type: String, required: true },
+      id: { type: String, required: true, unique: true },
+      hours: { type: Number, required: true },
+      image: { type: String },
+    },
+    { timestamps: true }
+  )
 );
 
 const Student_Course = mongoose.model(
-    "Student_Course",
-    new mongoose.Schema(
-        {
-            studentID: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "User",
-                required: true,
-            },
-            courseID: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Course",
-                required: true,
-            },
-            duration: { type: Number, required: true }, // In months
-        },
-        { timestamps: true }
-    )
+  "Student_Course",
+  new mongoose.Schema(
+    {
+      studentID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      courseID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+        required: true,
+      },
+      duration: { type: Number, required: true }, // In months
+    },
+    { timestamps: true }
+  )
 );
 
 const Instructor_Course = mongoose.model(
-    "Instructor_Course",
-    new mongoose.Schema(
-        {
-            instructorID: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "User",
-                required: true,
-            },
-            courseID: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Course",
-                required: true,
-            },
-            duration: { type: Number, required: true }, // In months
-        },
-        { timestamps: true }
-    )
+  "Instructor_Course",
+  new mongoose.Schema(
+    {
+      instructorID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      courseID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+        required: true,
+      },
+      duration: { type: Number, required: true }, // In months
+    },
+    { timestamps: true }
+  )
 );
 
 const Session = mongoose.model(
-    "Session",
-    new mongoose.Schema(
-        {
-            token: { type: String, required: true },
-            createDate: { type: Date, default: Date.now },
-        },
-        { timestamps: true }
-    )
+  "Session",
+  new mongoose.Schema(
+    {
+      token: { type: String, required: true },
+      createDate: { type: Date, default: Date.now },
+    },
+    { timestamps: true }
+  )
 );
 
 const Exam = mongoose.model(
-    "Exam",
-    new mongoose.Schema(
-        {
-            id: { type: String, required: true, unique: true },
-            courseID: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Course",
-                required: true,
-            },
-            title: { type: String, required: true },
-            startDate: { type: Date, required: true },
-            duration: { type: Number, required: true }, // Duration in minutes
-            endDate: { type: Date, required: true },
-            description: { type: String },
-        },
-        { timestamps: true }
-    )
+  "Exam",
+  new mongoose.Schema(
+    {
+      id: { type: String, required: true, unique: true },
+      courseID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+        required: true,
+      },
+      title: { type: String, required: true },
+      startDate: { type: Date, required: true },
+      duration: { type: Number, required: true }, // Duration in minutes
+      endDate: { type: Date, required: true },
+      description: { type: String },
+    },
+    { timestamps: true }
+  )
 );
 
 const Question = mongoose.model(
-    "Question",
-    new mongoose.Schema(
-        {
-            id: { type: String, required: true, unique: true },
-            title: { type: String, required: true },
-            answers: { type: [String], required: true },
-            correctAnswer: { type: String, required: true },
-            examID: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Exam",
-                required: true,
-            },
-        },
-        { timestamps: true }
-    )
+  "Question",
+  new mongoose.Schema(
+    {
+      id: { type: String, required: true, unique: true },
+      title: { type: String, required: true },
+      answers: { type: [String], required: true },
+      correctAnswer: { type: String, required: true },
+      examID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Exam",
+        required: true,
+      },
+    },
+    { timestamps: true }
+  )
 );
 
 const Assignment = mongoose.model(
-    "Assignment",
-    new mongoose.Schema(
-        {
-            id: { type: String, required: true, unique: true },
-            courseID: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Course",
-                required: true,
-            },
-            startDate: { type: Date, required: true },
-            duration: { type: Number, required: true }, // Duration in hours
-            endDate: { type: Date, required: true },
-            title: { type: String, required: true },
-            description: { type: String },
-            document: { type: String },
-        },
-        { timestamps: true }
-    )
+  "Assignment",
+  new mongoose.Schema(
+    {
+      id: { type: String, required: true, unique: true },
+      courseID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+        required: true,
+      },
+      startDate: { type: Date, required: true },
+      duration: { type: Number, required: true }, // Duration in hours
+      endDate: { type: Date, required: true },
+      title: { type: String, required: true },
+      description: { type: String },
+      document: { type: String },
+    },
+    { timestamps: true }
+  )
 );
 
 const AssignmentAnswer = mongoose.model(
-    "AssignmentAnswer",
-    new mongoose.Schema({
-        id: { type: String, required: true, unique: true },
-        document: { type: String, required: true },
-        assignmentID: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Assignment",
-            required: true,
-        },
-        studentID: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-        grade: { type: Number },
-    })
+  "AssignmentAnswer",
+  new mongoose.Schema({
+    id: { type: String, required: true, unique: true },
+    document: { type: String, required: true },
+    assignmentID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Assignment",
+      required: true,
+    },
+    studentID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    grade: { type: Number },
+  })
 );
 
 const StudentExam = mongoose.model(
-    "StudentExam",
-    new mongoose.Schema(
-        {
-            examID: {
-                type: String,
-                ref: "Exam",
-                required: true,
-            },
-            studentID: {
-                type: String,
-                ref: "User",
-                required: true,
-            },
-            grade: { type: Number },
-        },
-        { timestamps: true }
-    )
+  "StudentExam",
+  new mongoose.Schema(
+    {
+      examID: {
+        type: String,
+        ref: "Exam",
+        required: true,
+      },
+      studentID: {
+        type: String,
+        ref: "User",
+        required: true,
+      },
+      grade: { type: Number },
+    },
+    { timestamps: true }
+  )
+);
+
+const Announcement = mongoose.model(
+  "Announcement",
+  new mongoose.Schema({
+    id: { type: String, required: true, unique: true },
+    title: { type: String, required: true },
+    courseId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+    creatorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    createdAt: { type: Date, default: Date.now },
+    editedAt: { type: Date, default: null },
+    isEdited: { type: Boolean, default: false },
+  })
+);
+
+const Post = mongoose.model(
+  "Post",
+  new mongoose.Schema({
+    id: { type: String, required: true, unique: true },
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    AnnouncementId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Announcement",
+      required: true,
+    },
+    creatorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    isEdited: { type: Boolean, default: false },
+    createdAt: { type: Date, default: Date.now },
+    editedAt: { type: Date, default: null },
+  })
+);
+
+const Comment = mongoose.model(
+  "Comment",
+  new mongoose.Schema({
+    id: { type: String, required: true, unique: true },
+    postID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+    content: { type: String, required: true },
+    creatorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    createdAt: { type: Date, default: Date.now },
+    editedAt: { type: Date, default: null },
+  })
+);
+
+const Reply = mongoose.model(
+  "Reply",
+  new mongoose.Schema({
+    id: { type: String, required: true, unique: true },
+    commentID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+      required: true,
+    },
+    content: { type: String, required: true },
+    creatorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    createdAt: { type: Date, default: Date.now },
+    editedAt: { type: Date, default: null },
+  })
 );
 
 //--------------------------------------------
 
 module.exports = {
-    User,
-    Course,
-    Student_Course,
-    Instructor_Course,
-    Session,
-    Exam,
-    StudentExam,
-    Assignment,
-    AssignmentAnswer,
-    Question,
+  User,
+  Course,
+  Student_Course,
+  Instructor_Course,
+  Session,
+  Exam,
+  StudentExam,
+  Assignment,
+  AssignmentAnswer,
+  Question,
+  Announcement,
+  Post,
+  Comment,
+  Reply,
 };
--- BackEnd/router/ExamRouter.js
+++ BackEnd/router/ExamRouter.js
@@ -6,53 +6,53 @@ const router = Router();
 router.post("/createExam", verifyToken("Instructor"), Controller.createExam); //verified
 
 router.post(
-    "/addQuestions",
-    verifyToken("Instructor"),
-    Controller.addQuestions
+  "/addQuestions",
+  verifyToken("Instructor"),
+  Controller.addQuestions
 ); //verified
 
 router.get(
-    "/getExamsForAdmins",
-    verifyToken("Admin"),
-    Controller.getExamsforAdmins
+  "/getExamsForAdmins",
+  verifyToken("Admin"),
+  Controller.getExamsforAdmins
 ); //verified
 
 router.get(
-    "/getExamsForStudents/:studentId",
-    verifyToken("Student"),
-    Controller.getExamsforStudents
+  "/getExamsForStudents/:studentId",
+  verifyToken("Student"),
+  Controller.getExamsforStudents
 ); //verified
 
 router.get("/getExam/:examId", verifyToken(), Controller.StartSolvingExam); //verified
 
 router.put(
-    "/updateExam/:examId",
-    verifyToken("Instructor"),
-    Controller.updateExam
+  "/updateExam/:examId",
+  verifyToken("Instructor"),
+  Controller.updateExam
 ); //verified
 
 router.delete(
-    "/deleteExam/:examId",
-    verifyToken("Instructor"),
-    Controller.deleteExam
+  "/deleteExam/:examId",
+  verifyToken("Instructor"),
+  Controller.deleteExam
 ); //verified
 
 router.put(
-    "/updateQuestion/:questionId",
-    verifyToken("Instructor"),
-    Controller.updateQuestion
+  "/updateQuestion/:questionId",
+  verifyToken("Instructor"),
+  Controller.updateQuestion
 ); //verified
 
 router.delete(
-    "/deleteQuestion/:questionId",
-    verifyToken("Instructor"),
-    Controller.deleteQuestion
+  "/deleteQuestion/:questionId",
+  verifyToken("Instructor"),
+  Controller.deleteQuestion
 ); //verified
 
 router.post(
-    "/solveExam/:examId",
-    verifyToken("Student"),
-    Controller.finishSolvingExam
+  "/solveExam/:examId",
+  verifyToken("Student"),
+  Controller.finishSolvingExam
 ); //verified
 
 module.exports = router;
