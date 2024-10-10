eac2d58458fea024a41028ddcca4ea4c771672cd
Ahany42 <ahanyfathy42@gmail.com>
2024-10-11 00:17:44 +0300

    roll back to user card

--- FrontEnd/src/Components/CourseDetails/CourseDetails.jsx
+++ FrontEnd/src/Components/CourseDetails/CourseDetails.jsx
@@ -65,7 +65,6 @@ const CourseDetails = () => {
                     <span>Back</span>
                 </button>
                 <div className="card course-details card-shadow">
-                    {/* check if instructor is teaching this course */}
                     <div className="card-header details-header">
                         <h3 className="course-title alignLeft-text bold-text">{course.title}</h3>
                         <img src={ReactImg} alt="Course Photo"/>
