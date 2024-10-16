ef37665f6b28689a92f32007263ac2ef96693465
Eng-Khalid-Zaki <eng.khalid.zaki.2022@gmail.com>
2024-10-16 22:10:19 +0300

    edit questions ready for testing

--- FrontEnd/src/Components/MaterialCard/MaterialCard.jsx
+++ FrontEnd/src/Components/MaterialCard/MaterialCard.jsx
@@ -12,8 +12,10 @@ import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { getCookie } from "../Cookie/Cookie.jsx";
 import "./MaterialCard.css";
 import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router";
 
 const MaterialCard = ({ material }) => {
+  const navigate = useNavigate();
   const [editableExam, setEditableExam] = useState({});
   const [isEditing, setIsEditing] = useState(false);
   console.log(jwtDecode(getCookie("token")));
@@ -56,77 +58,12 @@ const MaterialCard = ({ material }) => {
       }
     }
   };
-  // const editExamHandler = async (material) => {
-  //   setEditableExam({
-  //     id: material.id,
-  //     title: material.title,
-  //     startDate: material.startDate,
-  //     dueDate: material.dueDate,
-  //     duration: material.duration,
-  //   });
-  //   setIsEditing(true);
-  // };
+  const editExamHandler = async (material) => {
+    navigate(`/examQuestions/${material.id}`);
+  };
 
-  //   try {
-  //     const date1 = new Date(Date.parse("2024-09-01T00:00:00.000+00:00"));
-  //     const date2 = new Date();
-  //     const milliseconds1 = date1.getTime();
-  //     const milliseconds2 = date2.getTime();
-  //     const totalMelliseconds = milliseconds2 + milliseconds1;
-  //     const differenceDate = new Date(totalMelliseconds);
-  //     const date3 = new Date(Date.parse("2024-09-30T00:00:00.000+00:00"));
-  //     const date4 = new Date();
-  //     const milliseconds3 = date3.getTime();
-  //     const milliseconds4 = date4.getTime();
-  //     const totalMelliseconds2 = milliseconds3 + milliseconds4;
-  //     const differenceDate2 = new Date(totalMelliseconds2);
-  //     console.log(differenceDate, differenceDate2);
-  //     const response = await fetch(
-  //       `http://localhost:3008/updateExam/${examId}`,
-  //       {
-  //         method: "PUT",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //           Authorization: `${getCookie("token")}`, // Ensure token is correctly fetched
-  //         },
-  //         body: JSON.stringify({
-  //           title: "Midterm Exam" + "test",
-  //           sDate: differenceDate,
-  //           duration: 90 + 10,
-  //           eDate: differenceDate2,
-  //         }),
-  //       }
-  //     );
-
-  //     const result = await response.json();
-
-  //     if (response.ok) {
-  //       setExams((prevExams) =>
-  //       prevExams.map((e) =>
-  //        e.id === editableExam.id
-  //        ? {
-  //       ...e,
-  //       title: editableExam.title,
-  //       courseID: courses.find((c) => c.title === editableExam.course)
-  //       ?.id,
-  //       dueDate: editableExam.dueDate,
-  //       description: editableExam.description,
-  //       }
-  //       : e
-  //       )
-  //       );
-  //       setIsEditing(false);
-  //       showMessage("Exam updated successfully", false);
-  //     } else {
-  //       showMessage(result.error, true);
-  //     }
-  //   } catch (error) {
-  //     console.error("Error updating exam:", error);
-  //     showMessage("Unexpected Error Occurred", true);
-  //   }
-  // };
-  const deleteAnnouncementHandler = async (ids) => {
-    let id = "32e33491-9827-4363-b4e4-a20362c83560";
+  const deleteAnnouncementHandler = async (material) => {
+    let id = material.id;
     let userId = jwtDecode(getCookie("token")).id;
     console.log(jwtDecode(getCookie("token")).id);
     const isConfirmed = await confirmationToast(
